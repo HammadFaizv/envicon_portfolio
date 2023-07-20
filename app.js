c5a71fb307f5160b450d6b44ff6a7c3285227c0f
HammadFaizv <hammadfaza12345@gmail.com>
2023-07-20 11:53:05 +0530

    mobile responsive

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const nodemailer = require("nodemailer");
+const dotenv = require('dotenv');
 
+dotenv.config();
 
 const app = express();
 app.use(express.urlencoded({extended:true}));
@@ -38,34 +40,28 @@ app.post("/contact", function(req, res){
         secure: true,
         auth: {
             type: 'OAuth2',
-            user: 'enviconsmpt@gmail.com',
-            clientId: '64728600704-qih716r56lmkvamflssd8qjifllh2b8d.apps.googleusercontent.com',
-            clientSecret: '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',
-            refreshToken: '1//04PnBBCyIZKsECgYIARAAGAQSNwF-L9IruKZuC-oGaD1_N_q7QkA-ge78dxjKCC5y5ApNPAeoPmUk9nz9anJv7fv1wCx7r0CUc5w',
-            accessToken: 'ya29.a0AbVbY6MFuC9A9NVvkiNtYShI4KIdCFFdB9T4G_p8sshZMxbWlZBtMSYlHlU-wWSmmEKNtWQe0-wL4qFvBW7rU_SCScoF-aUSVvtxBm1bq9szc8hnydgYHAopXC789nu6z5zIGwATcmFT3wj0TwAzxIseYNupaCgYKAXgSARMSFQFWKvPlCX8Nc3TX3uOwXiGdO71eSQ0163'
+            user: process.env.user,
+            clientId: process.env.clientId,
+            clientSecret: process.env.clientSecret,
+            refreshToken: process.env.refreshToken,
+            accessToken: process.env.accessToken,
         }
     });
 
     var mailOptions = {
-        from : "enviconsmpt@gmail.com",
-        to:"envicondxb@gmail.com",
+        from : process.env.user,
+        to: process.env.sendTo,
         subject:"contact form enquiry",
         text: "Name : " + name + "\nEmail : " + email + "\nPhone Number : " + number + "\nMessage : " + message 
     }
 
-    transporter.sendMail(mailOptions, function(req, res){
-        if(error){
-            send("error");
-        }else{
-            console.log
-        }
-    })
+    transporter.sendMail(mailOptions);
     
     res.redirect("/success");
-
-    
 })
 
-app.listen(process.env.PORT || 3000, function(){
-    console.log("server started on port 3000");
+const port = process.env.PORT || 3000;
+
+app.listen(port, function(){ 
+    console.log(`server started on port ${port}`);
 })
